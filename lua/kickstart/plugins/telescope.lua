12319c6757a32be742dc28bcbba33b95f0d65c0e
abdum99 <a.mohamdy99@gmail.com>
2024-08-09 20:55:27 -0700

    removed persisted

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -81,26 +81,25 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(telescope.load_extension, 'ui-select')
     -- …other Telescope extensions
     pcall(telescope.load_extension, 'ssh-config')
-    pcall(telescope.load_extension, 'persisted')
+    -- pcall(telescope.load_extension, 'persisted')
     pcall(telescope.load_extension, 'scope')
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    local persisted = telescope.extensions.persisted
+    -- local persisted = telescope.extensions.persisted
 
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
     vim.keymap.set('n', '<leader>ft', builtin.builtin, { desc = '[F]ind [T]elescope' })
 
-    vim.keymap.set('n', '<leader>fs', persisted.persisted, { desc = '[F]ind [S]essions' })
-
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
 
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
-    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+
+    vim.keymap.set('n', '<leader>f.', builtin.resume, { desc = '[F]ind Continue' })
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]ind [R]ecent Files' })
 
     -- map :Telescope ssh-config to a keymap
     vim.keymap.set('n', '<leader>fc', '<cmd>Telescope ssh-config<CR>', { desc = '[F]ind [C]onnection (ssh)' })

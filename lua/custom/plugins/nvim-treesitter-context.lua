f1eb6e92bd68bd4ab8f85a66f62e3ceb2acec321
abdum99 <a.mohamdy99@gmail.com>
2024-09-11 21:54:45 -0700

    minor

--- lua/custom/plugins/nvim-treesitter-context.lua
+++ lua/custom/plugins/nvim-treesitter-context.lua
@@ -2,7 +2,10 @@ return {
   'nvim-treesitter/nvim-treesitter-context',
   config = function()
     local treesitter_context = require 'treesitter-context'
-    treesitter_context.setup()
+    treesitter_context.setup {
+      multiline_threshold = 3,
+      mode = 'cursor',
+    }
 
     vim.keymap.set('n', '[c', function()
       treesitter_context.go_to_context(vim.v.count1)

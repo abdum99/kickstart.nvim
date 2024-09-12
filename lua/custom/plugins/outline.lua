94ac485e02779dbda51d0a042cb54a082fe78747
abdum99 <a.mohamdy99@gmail.com>
2024-09-11 21:49:42 -0700

    outline exclude terminating symbols

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -13,15 +13,33 @@ return {
       hide_cursor = true,
     },
     preview_window = {
-      auto_preview = true,
+      auto_preview = false,
       live = false,
       open_hover_on_preview = true,
     },
 
     keymaps = {
       close = { 'q' }, -- <Esc> shold not close outline
-      focus_code = { 'Esc' },
+      focus_code = { '<Esc>' },
       hover_symbol = { 'gh' },
+    },
+
+    symbols = {
+      filter = {
+        'Property',
+        'Field',
+        'Variable',
+        'String',
+        'Number',
+        'Boolean',
+        'Array',
+        'Object',
+        'Key',
+        'Null',
+        'EnumMember',
+        'Parameter',
+        exclude = true
+      },
     },
   },
 }

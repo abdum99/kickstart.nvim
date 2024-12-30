ab8492c0b978d914db2f3cd39a16bfea26f4ff59
abdum99 <a.mohamdy99@gmail.com>
2024-12-30 00:20:55 -0800

    minor: removed <esc><esc> mapping in terminal mode for lazygit

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -90,7 +90,7 @@ keymap.set('n', 'gq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]ui
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')

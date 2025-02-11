601e5cd9f25383237c92d8909739330ab784a2bc
abdum99 <a.mohamdy99@gmail.com>
2025-02-11 15:05:25 -0800

    theme

--- lua/plugins/plugins/themes.lua
+++ lua/plugins/plugins/themes.lua
@@ -1,19 +1,20 @@
-return {
-  -- {
-  -- 'projekt0n/github-nvim-theme',
-  -- name = 'github-theme',
-  -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  -- priority = 1000, -- make sure to load this before all the other start plugins
-  -- config = function()
-  --   require('github-theme').setup({
-  --     -- ...
-  --   })
-  --
-  --   -- vim.cmd('colorscheme github_dark_dimmed')
-  --   vim.cmd('colorscheme github_dark_default')
-  -- end,
-  -- },
-  {
+local themes = {
+  github = {
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+
+    -- vim.cmd('colorscheme github_dark_dimmed')
+    vim.cmd('colorscheme github_dark_default')
+  end,
+  },
+
+  ayu = {
     'Shatur/neovim-ayu',
     lazy = false,
     priority = 1000,
@@ -60,88 +61,104 @@ return {
       vim.cmd.colorscheme 'ayu'
     end,
   },
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     require('tokyonight').setup {
-  --       style = "night",
-  --     }
-  --     require('tokyonight').load()
-  --   end
-  --   -- init = function()
-  --   --   -- Load the colorscheme here.
-  --   --   -- Like many other themes, this one has different styles, and you could load
-  --   --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --   --   -- vim.cmd.colorscheme 'tokyonight-night'
-  --   --
-  --   --   -- You can configure highlights by doing something like:
-  --   --   -- vim.cmd.hi 'Comment gui=none'
-  --   -- end,
-  -- },
-  --
-  -- -- Using lazy.nvim
-  -- {
-  --   'ribru17/bamboo.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('bamboo').setup {
-  --       -- optional configuration here
-  --     }
-  --     require('bamboo').load()
-  --   end,
-  -- },
-  -- {
-  --   'ellisonleao/gruvbox.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     require('gruvbox').setup {
-  --       -- inverse = false,
-  --       -- contrast = 'soft',
-  --     }
-  --     require'gruvbox'.load()
-  --   end,
-  -- },
-  -- {
-  --   'navarasu/onedark.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('onedark').setup {
-  --       style = 'dark'
-  --     }
-  --     require'onedark'.load()
-  --   end
-  -- },
-  -- {
-  --   'sainnhe/sonokai',
-  --   lazy = false,
-  --   priority = 1000,
-  -- },
-  -- {
-  --   'marko-cerovac/material.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('material').setup {
-  --     }
-  --   end
-  -- },
-  -- {
-  --   "rebelot/kanagawa.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('kanagawa').setup {
-  --       style = 'dark'
-  --     }
-  --     require'kanagawa'.load('wave')
-  --   end
-  -- }
+
+  rosepine = {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    variant = "moon",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end
+  },
+
+  tokyonight = { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('tokyonight').setup {
+        style = "night",
+      }
+      require('tokyonight').load()
+    end
+    -- init = function()
+    --   -- Load the colorscheme here.
+    --   -- Like many other themes, this one has different styles, and you could load
+    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --   -- vim.cmd.colorscheme 'tokyonight-night'
+    --
+    --   -- You can configure highlights by doing something like:
+    --   -- vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
+
+  bamboo = {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {
+        -- optional configuration here
+      }
+      require('bamboo').load()
+    end,
+  },
+
+  gruvbox = {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup {
+        -- inverse = false,
+        -- contrast = 'soft',
+      }
+      require'gruvbox'.load()
+    end,
+  },
+
+  onedark = {
+    'navarasu/onedark.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        style = 'dark'
+      }
+      require'onedark'.load()
+    end
+  },
+
+  sonokai = {
+    'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
+  },
+
+  material = {
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('material').setup {
+      }
+    end
+  },
+
+  kanagawa = {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup {
+        style = 'dark'
+      }
+      require'kanagawa'.load('wave')
+    end
+  }
 }
+
+return themes['kanagawa']
